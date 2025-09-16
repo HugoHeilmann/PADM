4a8d5d70cf0c7937147e4faa046584a0e25ce7ce
Hugo Heilmann <hugo.heilmann@etu.univ-cotedazur.fr>
2025-09-16 14:52:01 +0200

    initial commit

---
+++ app/src/main/kotlin/com/example/piratesAuxDoigtsMouilles/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.piratesAuxDoigtsMouilles
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.runtime.*
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            PiratesAuxDoigtsMouillesApp()
+        }
+    }
+}
+
+@Composable
+fun PiratesAuxDoigtsMouillesApp() {
+}
